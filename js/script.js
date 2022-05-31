a10ad52ba05ce535a4b3544abe68f18f086cff24
javoxpl <liacovone7@outlook.it>
2022-05-31 16:51:14 +0200

    generatore numeri da scoprire

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ Dopo 30 secondi, nascondere i numeri.
 L’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite un prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
+
+
+/*generatore dei numeri random*/
+var numeriDaScoprire = [];
+
+while (numeriDaScoprire.length < 5) {
+  var numeroRandom = Math.floor(Math.random() * (100 - 1 + 1) ) + 1;
+if (!numeriDaScoprire.includes(numeroRandom)) {
+    numeriDaScoprire.push(numeroRandom);
+}
+}
+
+console.log(numeriDaScoprire);
+
+/*l'alert*/
+alert('Sbrigati,memorizza questi numeri, hai poco tempo! \nHai 30 secondi:' + '\n' + numeriDaScoprire);
+
